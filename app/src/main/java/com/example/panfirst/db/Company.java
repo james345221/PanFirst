938b94fea96628da5e3b060a64e508da4ff8ffa2
James <408758348@qq.com>
2017-06-05 19:05:02 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/panfirst/db/Company.java
@@ -0,0 +1,28 @@
+package com.example.panfirst.db;
+
+/**
+ * Created by asus-pc on 2017/6/5.
+ */
+
+public class Company {
+
+    private String com;
+    private String no;
+    public String getCom() {
+        return com;
+    }
+
+    public void setCom(String com) {
+        this.com = com;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+
+}
